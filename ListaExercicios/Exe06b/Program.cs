fd347d4d9c797d15fe9e58cf4da7d9cf0a312914
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-08-28 09:26:49 -0300

    lista

---
+++ ListaExercicios/Exe06b/Program.cs
@@ -0,0 +1,23 @@
+﻿int tamanho = 100;
+int[] vetor = new int[tamanho];
+
+Random random = new Random();
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = random.Next(1000);
+
+    for (int e = 0; e < vetor.Length; e++)
+    { //evitar numeros iguais
+        if (vetor[i] == vetor[e] && i != e)
+        {
+            vetor[i] = random.Next(1000);
+            e = 0;
+        }
+    }
+
+}
+
+Array.Sort(vetor);
+
+Console.WriteLine(string.Join(", ", vetor));
